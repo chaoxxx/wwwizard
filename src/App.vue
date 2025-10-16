cf19b3bffb63918dd2a6aac21a0f061b74295ec4
zhuchao <zhuchao1103@gmail.com>
2025-10-16 13:16:14 +0800

    修改软件窗口显示名称

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,7 @@ import BookManagement from './components/book/BookManagement.vue'; // 引入书�
 </script>
 
 <template>
-  <div>
-    <a href="https://www.electronjs.org/" target="_blank">
-      <img src="./assets/electron.svg" class="logo electron" alt="Electron logo" />
-    </a>
-    <a href="https://vitejs.dev/" target="_blank">
-      <img src="./assets/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <BookManagement />
-  <div class="flex-center">
-    Place static files into the <code>/public</code> folder
-    <img style="width: 2.4em; margin-left: .4em;" src="/logo.svg" alt="Logo">
-  </div>
 </template>
 
 <style>

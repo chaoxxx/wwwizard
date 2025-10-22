4563f53ff31ba60c15ec1d9d3e3947321d593946
zhuchao1103 <zhuchao1103@gmail.com>
2025-10-22 01:23:55 +0000

    修改sql文件路径

--- wwwizard.sql
+++ wwwizard.sql
@@ -3,46 +3,44 @@
 /*==============================================================*/
 create table if not exists www_book
 (
-   id                   varchar(20) not null comment '唯一标识',
-   book_title           text not null comment '书籍标题',
-   cover                text comment '封面路径',
-   book_type            varchar(2) not null comment '书籍类型',
-   book_desc            text comment '书籍描述',
-   book_words_count     bigint not null comment '字数统计',
-   create_time          datetime not null comment '创建时间',
-   update_time          datetime not null comment '最后编辑时间',
+   id                   varchar(20) not null ,
+   book_title           text not null ,
+   cover                text ,
+   book_type            varchar(2) not null ,
+   book_desc            text ,
+   book_words_count     bigint not null ,
+   create_time          datetime not null ,
+   update_time          datetime not null ,
    primary key (id)
 );
 
-alter table if not exists www_book comment '书本信息表';
+
 
 /*==============================================================*/
 /* Table: www_volume                                            */
 /*==============================================================*/
 create table if not exists www_volume
 (
-   id                   varchar(20) not null comment '唯一标识',
-   book_id              varchar(20) not null comment 'www_book唯一标识',
-   volume_title         text not null comment '卷标题',
-   volume_desc          text comment '卷描述',
-   create_time          datetime not null comment '创建时间',
+   id                   varchar(20) not null ,
+   book_id              varchar(20) not null ,
+   volume_title         text not null ,
+   volume_desc          text ,
+   create_time          datetime not null ,
    primary key (id)
 );
 
-alter table if not exists www_volume comment '书籍卷信息';
 
 /*==============================================================*/
 /* Table: www_chapter                                           */
 /*==============================================================*/
 create table if not exists www_chapter
 (
-   id                   varchar(20) not null comment '唯一标识',
-   book_id              varchar(20) not null comment 'www_book唯一标识',
-   volume_id            varchar(20) comment 'www_volume 唯一标识',
-   chapter_content      text comment '章节内容',
-   chapter_words_count  bigint not null comment '字数统计',
-   create_time          datetime not null comment '创建时间',
-   update_time          datetime not null comment '最后编辑时间'
+   id                   varchar(20) not null ,
+   book_id              varchar(20) not null ,
+   volume_id            varchar(20) ,
+   chapter_content      text ,
+   chapter_words_count  bigint not null ,
+   create_time          datetime not null ,
+   update_time          datetime not null
 );
 
-alter table if not exists www_chapter comment '章节';
